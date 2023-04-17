fe157e4859a5b67df87dbe6449c5929b86a5c8e1
Wissam Al Saub <wissam.alsaub01@stclairconnect.ca>
2023-04-17 07:12:51 -0400

    #41 Finished the detailed view: * now you can see a detailed view of the agent with his/her abilities * when the ability icon is clicked, the description text view will get updated accordingly

--- app/src/main/java/com/example/finalproject/UI/Agents/DetailedAgentFragment.java
+++ app/src/main/java/com/example/finalproject/UI/Agents/DetailedAgentFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -69,8 +70,9 @@ public class DetailedAgentFragment extends Fragment {
                              Bundle savedInstanceState) {
         // separated the view from the return statement
         View view = inflater.inflate(R.layout.fragment_detailed_agent, container, false);
-        //this can be used after we implement the data base
-      /*  ImageView agentImage = view.findViewById(R.id.agentFullImage);
+        // get all the views in the layout file
+
+        ImageView agentImage = view.findViewById(R.id.agentFullImage);
         TextView agentName = view.findViewById(R.id.detailedName);
         ImageView agentRole = view.findViewById(R.id.detailedRoleIcon);
         TextView agentDescription = view.findViewById(R.id.agentDescription);
@@ -79,17 +81,77 @@ public class DetailedAgentFragment extends Fragment {
         ImageView secondAbility = view.findViewById(R.id.secondAbility);
         ImageView thirdAbility = view.findViewById(R.id.thirdAbility);
         ImageView fourthAbility = view.findViewById(R.id.fourthAbility);
+        //add the ability to scroll through the description text view
+        abilityDescription.setMovementMethod(new ScrollingMovementMethod());
+
         if (getArguments() != null){
-            Picasso.get().load(agent.getAgentRole()).into(agentImage);
+            agent = getArguments().getParcelable(AGENT);
+            Picasso.get().load(agent.getAgentImage()).into(agentImage);
             agentName.setText(agent.getAgentName());
             Picasso.get().load(agent.getRoleImage()).into(agentRole);
             agentDescription.setText(agent.getAgentDescription());
-            abilityDescription.setText(agent.getFirstAbilityDescription());
             Picasso.get().load(agent.getFirstAbilityIcon()).into(firstAbility);
             Picasso.get().load(agent.getSecondAbilityIcon()).into(secondAbility);
             Picasso.get().load(agent.getThirdAbilityIcon()).into(thirdAbility);
             Picasso.get().load(agent.getFourthAbilityIcon()).into(fourthAbility);
-        }*/
+            // handle the ability icons click
+            firstAbility.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    firstAbility.setBackgroundColor(getResources().getColor(R.color.highlight));
+                    secondAbility.setBackgroundColor(0);
+                    thirdAbility.setBackgroundColor(0);
+                    fourthAbility.setBackgroundColor(0);
+                    firstAbility.setRotationX(-30);
+                    secondAbility.setRotationX(0);
+                    thirdAbility.setRotationX(0);
+                    fourthAbility.setRotationX(0);
+                    abilityDescription.setText(agent.getFirstAbilityDescription());
+                }
+            });
+            secondAbility.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    firstAbility.setBackgroundColor(0);
+                    secondAbility.setBackgroundColor(getResources().getColor(R.color.highlight));
+                    thirdAbility.setBackgroundColor(0);
+                    fourthAbility.setBackgroundColor(0);
+                    firstAbility.setRotationX(0);
+                    secondAbility.setRotationX(-30);
+                    thirdAbility.setRotationX(0);
+                    fourthAbility.setRotationX(0);
+                    abilityDescription.setText(agent.getSecondAbilityDescription());
+                }
+            });
+            thirdAbility.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    firstAbility.setBackgroundColor(0);
+                    secondAbility.setBackgroundColor(0);
+                    thirdAbility.setBackgroundColor(getResources().getColor(R.color.highlight));
+                    fourthAbility.setBackgroundColor(0);
+                    firstAbility.setRotationX(0);
+                    secondAbility.setRotationX(0);
+                    thirdAbility.setRotationX(-30);
+                    fourthAbility.setRotationX(0);
+                    abilityDescription.setText(agent.getThirdAbilityDescription());
+                }
+            });
+            fourthAbility.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    firstAbility.setBackgroundColor(0);
+                    secondAbility.setBackgroundColor(0);
+                    thirdAbility.setBackgroundColor(0);
+                    firstAbility.setRotationX(0);
+                    secondAbility.setRotationX(0);
+                    thirdAbility.setRotationX(0);
+                    fourthAbility.setRotationX(-30);
+                    fourthAbility.setBackgroundColor(getResources().getColor(R.color.highlight));
+                    abilityDescription.setText(agent.getFourthAbilityDescription());
+                }
+            });
+        }
         return view;
     }
 }
